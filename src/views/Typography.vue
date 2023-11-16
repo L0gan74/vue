6c78e0b0e2e56833839001b9983efccb810cdcb1
31011972Qw <ggtrfr@bk.ru>
2023-11-16 10:49:39 +0300

    vue

---
+++ src/views/Typography.vue
@@ -0,0 +1,25 @@
+<template>
+  <h1 class="heading-1">Типография</h1>
+  <h2 class="heading-2">Типография 2</h2>
+  <h2 class="heading-3">Типография 3</h2>
+</template>
+
+<style lang="scss">
+.heading {
+  &-1 {
+    font-size: 34px;
+    margin: 20px 0px;
+    font-weight: 300;
+  }
+  &-2 {
+    font-size: 22px;
+    margin: 20px 0px;
+    font-weight: 300;
+  }
+  &-3 {
+    font-size: 17px;
+    margin: 20px 0px;
+    font-weight: 300;
+  }
+}
+</style>
